6870e7a434c271f7f2d06392eddedbd1fa6b45d8
bonghwan <lbh6314@naver.com>
2021-02-12 15:25:52 +0900

    clonecoding_instagram

---
+++ clone_codling/photo/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
+from .models import Photo
+from django.http.response import HttpResponseRedirect, HttpResponseForbidden
+from django.contrib import messages
+
+class PhotoList(ListView):
+    model = Photo
+    template_name_suffix = '_list'
+
+class PhotoCreate(CreateView):
+    model = Photo
+    fields = ['text', 'image']
+    template_name_suffix = '_create'
+    success_url = '/'
+     
