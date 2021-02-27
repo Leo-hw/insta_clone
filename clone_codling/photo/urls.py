fa91526a8c1d39634b2827b4ae6cf773b8925b2c
leo <lbh6314@gmail.com>
2021-02-27 23:33:54 +0900

    update photo/urls.py
    added path for mylist, like, favorite, like_list, favorite_list

--- clone_codling/photo/urls.py
+++ clone_codling/photo/urls.py
@@ -4,8 +4,21 @@ from .views import *
 
 urlpatterns = [
     path('', PhotoList.as_view(), name='index'),
-    path('create/<int:pk>/', PhotoCreate.as_view(), name ='create'),
+    path('create/', PhotoCreate.as_view(), name ='create'),
+
+    # view my post.
+    path('mylist/', PhotoMyList.as_view(), name = 'mylist'),
+
+    # like and favorite
+    path('like/<int:photo_id>/', PhotoLike.as_view(), name = 'like'),
+    path('favorite/<int:photo_id>/', PhotoFavorite.as_view(), name = 'favorite'),
+
     path('update/<int:pk>/', PhotoUpdate.as_view(), name ='update'),
     path('delete/<int:pk>/', PhotoDelete.as_view(), name ='delete'),
     path('detail/<int:pk>/', PhotoDetail.as_view(), name ='detail'),
+
+    # view liked post and favorite 
+    path('like/', PhotoLikeList.as_view(), name = 'like_list'),
+    path('favorite/', PhotoFavoriteList.as_view(), name = 'like_list'),
+
 ]
